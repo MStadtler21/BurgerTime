09145a56e346bdc46e65f7b8ea22a59a274bbbb6
MatStat <powdersteamboat@gmail.com>
2020-08-27 14:21:21 -0600

    Database work

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE burgers_db;
+CREATE DATABASE burger;
 
-USE burgers_db;
+USE burger;
 
 CREATE TABLE burgers (
     id  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
